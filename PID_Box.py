ceecd6a34a6d9f778af038143b737a7b94e674a8
Scoot <56690169+mbjones73@users.noreply.github.com>
2021-04-12 10:40:21 -0400

    Update PID_Box.py

--- PID_Box.py
+++ PID_Box.py
@@ -14,6 +14,33 @@ double kd = 1
   
 
 
+void setup(){
+        setPoint = 0; 
+
+void loop(){
+        input = analogRead(A0);            
+        output = computePID(input);
+        delay(100);
+        analogWrite(3, output);              
+ 
+}
+```
+double computePID(double inp){     
+        currentTime = millis();           
+        elapsedTime = (double)(currentTime - previousTime);       
+        
+        error = Setpoint - inp;                               
+        cumError += error * elapsedTime;               
+        rateError = (error - lastError)/elapsedTime;   
+ 
+        double out = kp*error + ki*cumError + kd*rateError;                            
+ 
+        lastError = error;                                
+        previousTime = currentTime;                        
+
+        return out;                         
+
+```
 
 
     #This is where the you control it by the PID value
